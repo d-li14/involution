17dd2645be725095c8bbfc3d973d4a205b189f02
liduo.li <liduo.li@bytedance.com>
2021-01-31 21:47:24 +0800

    Add segmentation part

---
+++ seg/mmseg/models/necks/__init__.py
@@ -0,0 +1,4 @@
+from .fpn import FPN
+from .fpn_involution import FPN_involution
+
+__all__ = ['FPN', 'FPN_involution']
---
+++ seg/mmseg/models/utils/involution.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+from mmcv.cnn import ConvModule
+
+
+class involution(nn.Module):
+
+    def __init__(self,
+                 channels,
+                 kernel_size,
+                 stride):
+        super(involution, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.channels = channels
+        reduction_ratio = 4
+        self.group_channels = 16
+        self.groups = self.channels // self.group_channels
+        self.conv1 = ConvModule(
+            in_channels=channels,
+            out_channels=channels // reduction_ratio,
+            kernel_size=1,
+            conv_cfg=None,
+            norm_cfg=dict(type='BN'),
+            act_cfg=dict(type='ReLU'))
+        self.conv2 = ConvModule(
+            in_channels=channels // reduction_ratio,
+            out_channels=kernel_size**2 * self.groups,
+            kernel_size=1,
+            stride=1,
+            conv_cfg=None,
+            norm_cfg=None,
+            act_cfg=None)
+        if stride > 1:
+            self.avgpool = nn.AvgPool2d(stride, stride)
+        self.unfold = nn.Unfold(kernel_size, 1, (kernel_size-1)//2, stride)
+
+    def forward(self, x):
+        weight = self.conv2(self.conv1(x if self.stride == 1 else self.avgpool(x)))
+        b, c, h, w = weight.shape
+        weight = weight.view(b, self.groups, self.kernel_size**2, h, w).unsqueeze(2)
+        out = self.unfold(x).view(b, self.groups, self.group_channels, self.kernel_size**2, h, w)
+        out = (weight * out).sum(dim=3).view(b, self.channels, h, w)
+        return out
